16e3e78df3142fd726a5dfed031a77ee3f0b6e85
Guilherme Graça <GGracaSe@gmail.com>
2020-11-30 12:45:54 +0000

    Update remove_images.sh

--- remove_images.sh
+++ remove_images.sh
@@ -1,5 +1,5 @@
 IMAGES=$(doctl registry repository lt $1 --format Tag,UpdatedAt | tail -n +2 | sort -rk2 | awk '{print $1}')
-ALL_IMAGES=$(echo $IMAGES | tr '\n' ' ')
+ALL_IMAGES=$(echo $IMAGES | grep -v latest | tr '\n' ' ')
 OLD_IMAGES=$(echo $IMAGES | grep -v latest | tail -n +$2 | tr '\n' ' ')
 echo All Images: $ALL_IMAGES
 echo Old Images: $OLD_IMAGES
