2967c8cc77c56fb362f64c018bfbf775ebfdae0b
Guilherme Graça <GGracaSe@gmail.com>
2020-11-30 12:50:47 +0000

    Update remove_images.sh

--- remove_images.sh
+++ remove_images.sh
@@ -1,2 +1,3 @@
-IMAGES=$(doctl registry repository lt $1 --format Tag,UpdatedAt | tail -n +2 | sort -rk2 | awk '{print $1}' | grep -v latest | tail -n +$2 | tr '\n' ' ')
-echo $IMAGES
+TAGS=$(doctl registry repository lt $1 --format Tag,UpdatedAt | tail -n +2 | sort -rk2 | awk '{print $1}' | grep -v latest | tail -n +$2 | tr '\n' ' ')
+echo Tags to be removed: $TAGS
+[[ -n $TAGS ]] && doctl registry repository dt $1 $TAGS --force; true
